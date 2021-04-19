3faf9ce9862aeec13ad271e829b2929c1f23ece2
Joanne P Silva <80950389+JoannePSilva@users.noreply.github.com>
2021-04-19 07:27:06 -0300

    contador de caracteres
    Contar os caracteres sem os espaços

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,21 @@
-$(".frase");
 var frase = $(".frase").text();
-var frase = $(".frase").text();
-frase.split(" ");
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-
+var numPalavras  = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
+
 tamanhoFrase.text(numPalavras);
+
 
 var campo = $(".campo-digitacao");
 campo.on("input", function() {
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    console.log(qtdPalavras);
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
 
 });
-
-var conteudo = campo.val();
-var qtdPalavras = conteudo.split(/\S+/).length - 1;
-$("#contador-palavras").text(qtdPalavras);
-
-var qtdCaracteres = conteudo.length;
-$("#contador-caracteres").text(qtdCaracteres);
-
-var conteudo = campo.val();
 
 //Retira os espaço da String 
 var conteudoSemEspaco = conteudo.replace(/\s+/g,'');
