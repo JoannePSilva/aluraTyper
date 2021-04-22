f73f76e03de239c9c3c7176371e8737850c1f714
Joanne P Silva <joannepassos@gmail.com>
2021-04-22 14:18:44 -0300

    mudando as pastas
    separação das funções

--- public/js/main.js
+++ public/js/main.js
@@ -72,40 +72,9 @@ function reiniciaJogo() {
     campo.removeClass("borda-verde");
 };
 
-function inserePlacar() {
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Seu-nome";
-    var numPalavras = $("#contador-palavras").text();
-
-    var linha = novaLinha(usuario, numPalavras);
-
-    corpoTabela.append(linha);
-}
-function finalizaJogo() {
+}function finalizaJogo() {
     campo.attr("disabled", true);
     campo.toggleClass("campo-desativado");
     inserePlacar();
 }
 
-function novaLinha(usuario,palavras) {
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").attr("href","#").addClass("botao-remover");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    // Ícone dentro do <a>
-    link.append(icone);
-
-    // <a> dentro do <td>
-    colunaRemover.append(link);
-
-    // Os três <td> dentro do <tr>
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
